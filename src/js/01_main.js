8cf9d08cffc8a98ff643ba54dd767b6bc0210d66
aspartaue <spartakvasilev1999@gmail.com>
2023-06-19 21:08:56 +0900

    sadasd

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -36,68 +36,74 @@ var swiperMain = new Swiper(".mySwiperMain", {
 
 
 // Аккардион Операции
-function CustomSelect(options) {
-  var elem = options.elem;
-
-  elem.onclick = function(event) {
-    if (event.target.className == 'title') {
-      toggle();
-    } else if (event.target.tagName == 'LI') {
-      setValue(event.target.innerHTML, event.target.dataset.value);
-      close();
-    }
-  }
-
-  var isOpen = false;
-
-  // ------ обработчики ------
-
-  // закрыть селект, если клик вне его
-  function onDocumentClick(event) {
-    if (!elem.contains(event.target)) close();
-  }
-
-  // ------------------------
-
-  function setValue(title, value) {
-    elem.querySelector('.title').innerHTML = title;
-
-    var widgetEvent = new CustomEvent('select', {
-      bubbles: true,
-      detail: {
-        title: title,
-        value: value
+if (document.querySelector('.customselect')) {
+  function CustomSelect(options) {
+    var elem = options.elem;
+  
+    elem.onclick = function(event) {
+      if (event.target.className == 'title') {
+        toggle();
+      } else if (event.target.tagName == 'LI') {
+        setValue(event.target.innerHTML, event.target.dataset.value);
+        close();
       }
-    });
-
-    elem.dispatchEvent(widgetEvent);
-
+    }
+  
+    var isOpen = false;
+  
+    // ------ обработчики ------
+  
+    // закрыть селект, если клик вне его
+    function onDocumentClick(event) {
+      if (!elem.contains(event.target)) close();
+    }
+  
+    // ------------------------
+  
+    function setValue(title, value) {
+      elem.querySelector('.title').innerHTML = title;
+  
+      var widgetEvent = new CustomEvent('select', {
+        bubbles: true,
+        detail: {
+          title: title,
+          value: value
+        }
+      });
+  
+      elem.dispatchEvent(widgetEvent);
+  
+    }
+  
+    function toggle() {
+      if (isOpen) close()
+      else open();
+    }
+  
+    function open() {
+      elem.classList.add('open');
+      document.addEventListener('click', onDocumentClick);
+      isOpen = true;
+    }
+  
+    function close() {
+      elem.classList.remove('open');
+      document.removeEventListener('click', onDocumentClick);
+      isOpen = false;
+    }
+  
   }
-
-  function toggle() {
-    if (isOpen) close()
-    else open();
-  }
-
-  function open() {
-    elem.classList.add('open');
-    document.addEventListener('click', onDocumentClick);
-    isOpen = true;
-  }
-
-  function close() {
-    elem.classList.remove('open');
-    document.removeEventListener('click', onDocumentClick);
-    isOpen = false;
-  }
-
+  var animalSelect = new CustomSelect({
+    elem: document.getElementById('animal-select')
+  });
 }
 
-var animalSelect = new CustomSelect({
-  elem: document.getElementById('animal-select')
+
+
+
+$('.menu-btn').on('click', function() {
+  $('.logo').toggleClass('hidden');
 });
-
-
 
 $(function() {
   var Accordion = function(el, multiple) {
